d3e30b7371af68fa825487fe561184a9f9cfd5a1
Taeho Kang <tkang1@gmail.com>
2021-02-23 11:25:34 +0900

    apply auth ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
 import '../styles/globals.css'
+import React from 'react';
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from '../src/aws-exports';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+Amplify.configure(awsconfig);
+
+const AuthStateApp = ({ Component, pageProps }) => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData)
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+    <div className="App">
+      <Component {...pageProps} user={user} />
+      <AmplifySignOut />
+    </div>
+  ) : (
+    <AmplifyAuthenticator />
+  );
 }
 
-export default MyApp
+export default AuthStateApp;
